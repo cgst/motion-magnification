f83960431a5e4d40f31822466e3b20d6378cdc37
cgst <strat.cristian@gmail.com>
2019-12-01 06:47:43 +0000

    Make dataset map-style

--- deepmag/dataset.py
+++ deepmag/dataset.py
@@ -2,23 +2,19 @@ import os
 
 import numpy as np
 import torch
-from torchvision.datasets import VisionDataset
+from torchvision import transforms, datasets
 from torchvision.datasets.folder import default_loader
 
 
-class DeepMagDataset(VisionDataset):
-    def __init__(self, root, image_loader=default_loader):
-        super().__init__(root, transforms=None)
+class MotionMag(datasets.VisionDataset):
+    def __init__(self, root, image_loader=default_loader, transform=None):
+        super().__init__(root, transform=transform)
         amp_path = os.path.join(self.root, "train_mf.txt")
         self.amp_f = torch.from_numpy(np.loadtxt(amp_path))
         self.image_loader = image_loader
 
-    def _load_image(self, path):
-        im = self.image_loader(path)
-        return np.array(im) / 127.5 - 1.0
-
     def __len__(self):
-        return len(self.amp)
+        return len(self.amp_f)
 
     def __getitem__(self, index):
         fname = "%06d.png" % index
@@ -26,5 +22,20 @@ class DeepMagDataset(VisionDataset):
                  os.path.join(self.root, "frameB", fname),
                  os.path.join(self.root, "frameC", fname),
                  os.path.join(self.root, "amplified", fname))
-        frame_a, frame_b, frame_perturbed, target = map(self._load_image, paths)
-        return (frame_a, frame_b, frame_perturbed, self.amp_f[index]), target
+        frame_a, frame_b, frame_perturbed, target = map(self._im_load, paths)
+        return {'frame_a': frame_a,
+                'frame_b': frame_b,
+                'frame_perturbed': frame_perturbed,
+                'amplification_f': self.amp_f[index],
+                'frame_amplified': target}
+
+    def _im_load(self, path):
+        im = self.image_loader(path)
+        if self.transform:
+            im = self.transform(im)
+        return im
+
+
+def from_dir(root_dir):
+    ds = MotionMag(root_dir, transform=transforms.ToTensor())
+    return ds
